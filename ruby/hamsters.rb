0e75ab6cf18819d8ab9415f00dfeeaddade036c6
jakezappin <jake.zappin@gmail.com>
2016-05-29 12:05:20 -0400

    Section 4.2, Release 2 complete

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,20 +1,25 @@
 puts "What is the hamster's name?"
-name = gets
+name = gets.chomp
 
 puts "What is the volume level of the hamster?"
-volume = gets
+volume = gets.chomp
+volume = volume.to_i
 
 puts "What is the fur color of the hamster?"
-furColor = gets
+furColor = gets.chomp
 
 puts "Is the hamster a good candidate for adoption? (Y/N)"
-adoption = gets
+adoption = gets.chomp
+
+if(adoption == "Y")
+	adoption = true
+else
+	adoption = false
+end
 
 puts "What is the estimated age of the hamster?"
-age = gets
+age = gets.chomp
 
 if(age.length == 0)
 	age = nil
 end
-
-puts name + " " + volume + " " + furColor + " " + adoption + " " + age
