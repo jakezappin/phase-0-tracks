8f30ee628b7b7bfefad780993070bf78ecbd05de
jakezappin <jake.zappin@gmail.com>
2016-05-29 12:36:32 -0400

    Section 4.4, Release 3 Complete

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,67 +1,79 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees do you want to check?"
+numEmployees = gets.chomp
+numEmployees = numEmployees.to_i
 
-puts "How old are you?"
-age = gets.chomp
-age = age.to_i
+iterator = 0;
 
-puts "What year were you born?"
-birthYear = gets.chomp
-birthYear = birthYear.to_i
+while (iterator < numEmployees)
 
-puts "Our company cafeteria serves garlic bread.  Should we order some for you? (Y/N)"
-garlicBread = gets.chomp
+	puts "What is your name?"
+	name = gets.chomp
 
-if(garlicBread == "Y")
-	garlicBread = true
-else
-	garlicBread = false
-end
+	puts "How old are you?"
+	age = gets.chomp
+	age = age.to_i
 
-puts "Would you like to enroll in the company's health insurance? (Y/N)"
-healthInsurance = gets.chomp
+	puts "What year were you born?"
+	birthYear = gets.chomp
+	birthYear = birthYear.to_i
 
-if(healthInsurance == "Y")
-	healthInsurance = true
-else
-	healthInsurance = false
-end
+	puts "Our company cafeteria serves garlic bread.  Should we order some for you? (Y/N)"
+	garlicBread = gets.chomp
 
-
-#Return value
-isVampire = "Results inconclusive"
-
-#Check age v. birth year
-ageIsRight = false;
-ageCalc = 2016 - birthYear
-
-if(age == ageCalc)
-	ageIsRight = true
-elsif (age == ageCalc - 1)
-	ageIsRight = true;
-end
-
-#Logic
-if(ageIsRight == true)
-	if(garlicBread == true || healthInsurance == true)
-		isVampire = "Probably not a vampire"
+	if(garlicBread == "Y")
+		garlicBread = true
+	else
+		garlicBread = false
 	end
-end
 	
-if(ageIsRight == false)
-	if(garlicBread == false || healthInsurance == false)
-		isVampire = "Probably a vampire"
+	puts "Would you like to enroll in the company's health insurance? (Y/N)"
+	healthInsurance = gets.chomp
+	
+	if(healthInsurance == "Y")
+		healthInsurance = true
+	else
+		healthInsurance = false
 	end
-end
 	
-if(ageIsRight == false)
-	if(garlicBread == false && healthInsurance == false)
-		isVampire = "Almost certainly a vampire"
+	
+	#Return value
+	isVampire = "Results inconclusive"
+	
+	#Check age v. birth year
+	ageIsRight = false;
+	ageCalc = 2016 - birthYear
+	
+	if(age == ageCalc)
+		ageIsRight = true
+	elsif (age == ageCalc - 1)
+		ageIsRight = true;
 	end
-end
 	
-if(name == "Drake Cula" || name == "Tu Fang")
-	isVampire = "Definitely a vampire"
+	#Logic
+	if(ageIsRight == true)
+		if(garlicBread == true || healthInsurance == true)
+			isVampire = "Probably not a vampire"
+		end
+	end
+		
+	if(ageIsRight == false)
+		if(garlicBread == false || healthInsurance == false)
+			isVampire = "Probably a vampire"
+		end
+	end
+		
+	if(ageIsRight == false)
+		if(garlicBread == false && healthInsurance == false)
+			isVampire = "Almost certainly a vampire"
+		end
+	end
+		
+	if(name == "Drake Cula" || name == "Tu Fang")
+		isVampire = "Definitely a vampire"
+	end
+	
+	puts isVampire
+	iterator = iterator + 1
 end
 
-puts isVampire
+puts "The program is over!"
