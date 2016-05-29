f8bba7a2f2e29f4261f7249033bb3943e39d23ed
jakezappin <jake.zappin@gmail.com>
2016-05-29 12:30:55 -0400

    Section 4.4, Release 2 complete

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -26,3 +26,42 @@ if(healthInsurance == "Y")
 else
 	healthInsurance = false
 end
+
+
+#Return value
+isVampire = "Results inconclusive"
+
+#Check age v. birth year
+ageIsRight = false;
+ageCalc = 2016 - birthYear
+
+if(age == ageCalc)
+	ageIsRight = true
+elsif (age == ageCalc - 1)
+	ageIsRight = true;
+end
+
+#Logic
+if(ageIsRight == true)
+	if(garlicBread == true || healthInsurance == true)
+		isVampire = "Probably not a vampire"
+	end
+end
+	
+if(ageIsRight == false)
+	if(garlicBread == false || healthInsurance == false)
+		isVampire = "Probably a vampire"
+	end
+end
+	
+if(ageIsRight == false)
+	if(garlicBread == false && healthInsurance == false)
+		isVampire = "Almost certainly a vampire"
+	end
+end
+	
+if(name == "Drake Cula" || name == "Tu Fang")
+	isVampire = "Definitely a vampire"
+end
+
+puts isVampire
