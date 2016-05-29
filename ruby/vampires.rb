fcabf04eeec44c4284d8b00872ff9323bddb578f
jakezappin <jake.zappin@gmail.com>
2016-05-29 12:16:07 -0400

    Section 4.4, Release 1 complete

---
+++ ruby/vampires.rb
@@ -0,0 +1,24 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+age = age.to_i
+
+puts "Our company cafeteria serves garlic bread.  Should we order some for you? (Y/N)"
+garlicBread = gets.chomp
+
+if(garlicBread == "Y")
+	garlicBread = true
+else
+	garlicBread = false
+end
+
+puts "Would you like to enroll in the company's health insurance? (Y/N)"
+healthInsurance = gets.chomp
+
+if(healthInsurance == "Y")
+	healthInsurance = true
+else
+	healthInsurance = false
+end
