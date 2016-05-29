820179d17a0e96be56d557c298d94b3ce5232d4b
jakezappin <jake.zappin@gmail.com>
2016-05-29 15:00:07 -0400

    Section 4.6, Release 2 and 3 Complete

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,63 @@
+## encrypt() method
+
+## 1. Take a string
+## 2. Loop through each character in the string 
+## 3. At each iteration increase the character by one
+## 4. If the character is a "z", this is an edge case.  Change to "a"
+## 5. Return the new string
+
+def encrypt(str)
+	
+	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVQXYZ"
+	i = 0
+	
+	while i < str.length
+	
+		if(str[i] == "z")
+			str[i] = "a"
+		elsif(str[i] == "Z")
+			str[i] = "A"
+		else
+			position = alphabet.index(str[i])
+			str[i] = alphabet[position+1]
+		end
+		
+		i = i+1
+		
+	end
+	
+	return str
+	
+end
+
+## decrypt() method
+
+## 1. Take a string
+## 2. Loop through each character in the string 
+## 3. At each iteration decrease the character by one
+## 4. If the character is a "a", this is an edge case.  Change to "Z"
+## 5. Return the new string
+
+def decrypt(str)
+	
+	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVQXYZ"
+	i = 0
+	
+	while i < str.length
+	
+		if(str[i] == "a")
+			str[i] = "z"
+		elsif(str[i] == "A")
+			str[i] = "Z"
+		else
+			position = alphabet.index(str[i])
+			str[i] = alphabet[position-1]
+		end
+		
+		i = i+1
+		
+	end
+	
+	return str
+	
+end
