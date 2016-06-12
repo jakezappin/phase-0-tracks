bd8f03ab29ef3a85f2a992cabf4aab62b85bb0a8
jakezappin <jake.zappin@gmail.com>
2016-06-12 11:36:39 -0400

    GPS 2.2, Release 1

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,59 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # Create hash data structure.
+  # Split the string into seperate items.
+  # Add items to the hash structure.
+  # loop through and set default quantity
+  # print the list to the console using the print method
+# output: a hash table 
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: loop through the list to see if item is already included in hash structure
+  
+ # IF it is
+ 	# Add the quantity of the item to the default quantity
+ # Else
+ 	# Add the item and quantity
+ # End
+
+# print the list to console using the print method
+# output: a hash table including the new item 
+
+# Method to remove an item from the list
+# input: item name and optional quantity
+# steps: loop through the hash table to see if the item is present 
+
+ # IF the key is equal to the item name
+ 	# reduce quantity value from the key item 
+ 	# ELSE 
+ 	# print an error message
+ # End
+
+# print the list to console using the print method
+# output: a hash table 
+
+# Method to update the quantity of an item
+# input: item name and optional quantity 
+# steps: loop through the hash table to see if the item is present 
+
+ # IF the key is equal to the item name
+ 	# add value to the quantity
+ # ELSE
+ 	# print an error message
+ # End
+
+# print the list to console using the print method
+# output: a hash table with the new value included
+
+# Method to print a list and make it look pretty
+# input: the hash table name
+# steps: loop through and print out key value pairs
+# output: a hash table with all the key value pairs
+
+
+
+
+
+
